cadb20c63ac2eaed3a251daa6eb8a3f71d38b811
sergeybabann <sergeybabann@yandex.ru>
2024-10-04 22:07:10 +0300

    add 2 lesson

---
+++ 02_ways_to_declare_variables/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let user = 'Alex', 
+    age = 25, 
+    email = '1@my.ru' 
+
+    age = 17
+    email = 'mymail@my.ru'
+
+    age = 'Возраст 17 лет'
+    email = 0
+
+    email = age
+
+    const PI = 3.1415
+    PI = 2
+
+
+console.log(PI)
